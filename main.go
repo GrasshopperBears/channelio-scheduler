dc12ddd89b9416bcd58265ade5d22ecea9783a4d
GrasshopperBears <starday456@naver.com>
2022-05-12 18:31:25 +0900

    check webhook token validity

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+
+	"github.com/joho/godotenv"
+)
+
+func initialize() {
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Environment configuration failed.")
+  }
+}
 
 func main() {
+  initialize()
   app := fiber.New()
 
   app.Post("/", func(ctx *fiber.Ctx) error {
+    if ctx.Query("token") != os.Getenv("TOKEN") {
+      log.Println("Request with different token")
+      return ctx.SendStatus(400)
+    }
     return ctx.SendString("Hello, World!")
   })
 
