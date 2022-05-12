c23b16d465072add26ff64af140e0de55064f34e
GrasshopperBears <starday456@naver.com>
2022-05-13 02:54:38 +0900

    querying schedule model

--- services/getSchedule.go
+++ services/getSchedule.go
@@ -1,11 +1,18 @@
 package services
 
 import (
+	"server/models"
 	"server/structs"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func GetSchedule(ctx *fiber.Ctx, event *structs.WebhookEvent) error {
+	db := models.DB
+	var schedules []models.Schedule
+
+	result := db.Where("channel_id = ?", event.Entity.ChatId).Find(&schedules)
+	println(result.RowsAffected)
+
 	return ctx.SendStatus(200)
 }
