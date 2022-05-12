626a6e4472ec3a34c68fdb25b5b5292fb4b3382c
GrasshopperBears <starday456@naver.com>
2022-05-13 02:42:33 +0900

    add uuid and auto-generate using hook

--- models/scheduleSchema.go
+++ models/scheduleSchema.go
@@ -3,13 +3,23 @@ package models
 import (
 	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Schedule struct {
-	gorm.Model
+	ID uuid.UUID `gorm:"primaryKey"`
+  CreatedAt time.Time
+  DeletedAt gorm.DeletedAt `gorm:"index"`
 	ChannelId string `gorm:"index"`
 	Title string
 	Datetime time.Time `gorm:"index"`
 	IsTimeSet bool
+}
+
+func (schedule *Schedule) BeforeCreate(tx *gorm.DB) (err error) {
+	schedule.ID = uuid.New()
+	schedule.CreatedAt = time.Now()
+
+	return
 }
