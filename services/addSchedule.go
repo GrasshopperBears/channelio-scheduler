aa1a3cf7474e11c12e7975b90931a42d845319d0
Jinwoo Lee <starday456@kaist.ac.kr>
2022-05-14 02:19:39 +0900

    delete related history when create schedule

--- services/addSchedule.go
+++ services/addSchedule.go
@@ -11,11 +11,13 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
 
 var regex = regexp.MustCompile(texts.SCHEDULER_PREFIX + " +" + texts.SCHEDULER_ADD + "(?: +(?P<year>[0-9]{4})년)? +(?P<month>[0-9]{1,2})월 +(?P<date>[0-9]{1,2})일 +(?P<hour>[0-9]{1,2})시 +(?P<minute>[0-9]{1,2})분 (?P<title>.+)")
 
 func AddSchedule(ctx *fiber.Ctx, event *structs.WebhookEvent) error {
+	db := models.DB
 	match := regex.FindStringSubmatch(event.Entity.PlainText)
 	parseMap := utils.ParseRegexFind(regex, match)
 
@@ -34,12 +36,17 @@ func AddSchedule(ctx *fiber.Ctx, event *structs.WebhookEvent) error {
 	newSchedule.Title = title
 	newSchedule.IsTimeSet = true
 
-	result := models.DB.Create(&newSchedule)
-	
-	if result.Error != nil {
-		log.Println("Error: ", result.Error)
-		return ctx.SendStatus(500)
-	}
+	db.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Create(&newSchedule).Error; err != nil {
+			log.Println("Database error: ", err)
+			return err
+		}
+		if err := tx.Where("channel_id = ?", event.Entity.ChatId).Delete(&models.GetScheduleHistory{}).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+
 	log.Println("Schedule created")
 
 	return ctx.SendStatus(200)
