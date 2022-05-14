658ebd24163cafef79a888ded35ea0699c36fb8f
Jinwoo Lee <starday456@kaist.ac.kr>
2022-05-15 03:42:24 +0900

    add input validation

--- services/addSchedule.go
+++ services/addSchedule.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"fmt"
 	"log"
 	"regexp"
 	"server/models"
@@ -24,6 +25,15 @@ var addRegex = regexp.MustCompile(
 	"(?P<title>.+)",
 )
 
+func isValidDatetimeInput(year int, month int, date int, hour int, minute int) bool {
+	if date == 0 || (month == 0 && year != 0) { return false }
+	if (year > 0 && year < 2000) || year > 9999 || month < 0 || month > 12 || date < 0 || date > 31 ||
+		hour < 0 || hour > 24 || minute < 0 || minute >= 60 {
+			return false
+	}
+	return true
+}
+
 func AddSchedule(ctx *fiber.Ctx, event *structs.WebhookEvent) error {
 	db := models.DB
 	match := addRegex.FindStringSubmatch(event.Entity.PlainText)
@@ -36,13 +46,25 @@ func AddSchedule(ctx *fiber.Ctx, event *structs.WebhookEvent) error {
 	minute, _ := strconv.Atoi(parseMap["minute"])
 	title := parseMap["title"]
 
+	if !isValidDatetimeInput(year, month, date, hour, minute) {
+		block := structs.Block{Type: "text", Value: texts.MESSAGE_WRONG_FORMAT}
+		PostChannelMessage([]structs.Block{block}, []string{"silent"}, event.Entity.ChatType, event.Entity.ChatId)
+		return ctx.SendStatus(200)
+	}
+
+	today := time.Now()
+	if year == 0 {
+		year = today.Year()
+		if month < int(today.Month()) || month == int(today.Month()) { year++ }
+	}
+
 	datetime := time.Date(year, time.Month(month), date, hour, minute, 0, 0, time.Local)
 
 	newSchedule := models.Schedule{}
 	newSchedule.ChannelId = event.Entity.ChatId
 	newSchedule.Datetime = datetime
 	newSchedule.Title = title
-	newSchedule.IsTimeSet = true
+	newSchedule.IsTimeSet = parseMap["hour"] != ""
 
 	db.Transaction(func(tx *gorm.DB) error {
 		if err := tx.Create(&newSchedule).Error; err != nil {
